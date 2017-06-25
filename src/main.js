b491107543191fce1c4f35437b71de543284ff5d
储笑 <cx@chuxiaodeMacBook.local>
2017-06-25 13:35:54 +0800

    fixed

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
 
 import '@/assets/styles/common.less'
 
-import 'normalize.css'
 import 'github-markdown-css'
 
 Vue.config.productionTip = false
+
+Vue.use(iView)
 
 new Vue({
   el: '#app',
