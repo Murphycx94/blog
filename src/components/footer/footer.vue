acb0f9ab921f30c6fb8f7a12a713777eef6b7f02
储笑 <cx@chuxiaoeMacBook.lan>
2017-06-27 14:30:35 +0800

    fixed

--- src/components/footer/footer.vue
+++ src/components/footer/footer.vue
@@ -1,6 +1,11 @@
 <template>
-  <div id="footer">
-		底部
+  <div class="footer">
+		<p>使用Vue.js 基于GitHub构建</p>
+		<p>Copyright © 2017 MurphyC. All Rights Reserved</p>
+		<p><a href="mailto:shmilyclxy@163.com" title="shmilyclxy@163.com">shmilyclxy@163.com</a></p>
+		<p>
+			<a href="#">source</a> | <a href="#">GitHub</a>
+		</p>
   </div>
 </template>
 
