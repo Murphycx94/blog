e3b2af90ba3945cbaf4134c09ded5aa5d713553c
MurphyC <shmilyclxy@163.com>
2017-07-16 23:45:53 +0800

    fixed

--- src/components/footer/footer.vue
+++ src/components/footer/footer.vue
@@ -1,12 +1,12 @@
 <template>
-  <footer class="footer">
+	<footer class="footer">
 		<p>使用Vue.js 基于GitHub构建</p>
 		<p>Copyright © 2017 MurphyC. All Rights Reserved</p>
 		<p>tel：<a href="tel:17764526709" title="17764526709">17764526709</a></p>
 		<p>
 			<a href="#">source</a> | <a href="#">GitHub</a>
 		</p>
-  </footer>
+	</footer>
 </template>
 
 <script>
