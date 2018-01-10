08f5a9f25c52727b10ee03c09d75b1b8ce206e3d
MurphyC <shmilyclxy@163.com>
2018-01-10 16:25:01 +0800

    fixed

--- src/components/footer/footer.vue
+++ src/components/footer/footer.vue
@@ -1,10 +1,15 @@
 <template>
 	<footer class="footer">
 		<p>使用Vue.js 基于GitHub构建</p>
-		<p>Copyright © 2017 MurphyC. All Rights Reserved</p>
-		<p>tel：<a href="tel:17764526709" title="17764526709">17764526709</a></p>
 		<p>
-			<a href="https://github.com/Murphycx94/blog">Source</a> | <a href="https://github.com/Murphycx94">GitHub</a>
+			<a href="https://github.com/Murphycx94/blog">Source</a> | 
+			<a href="https://github.com/Murphycx94">GitHub</a> | 
+			<a href="tel:17764526709" title="17764526709">17764526709</a> | 
+			<a href="mailto:shmilyclxy@163.com">shmilyclxy@163.com</a>
+		</p>
+		<p>
+			<img src="./beian.png" alt="备案号">
+			浙ICP备17003605号-2
 		</p>
 	</footer>
 </template>
