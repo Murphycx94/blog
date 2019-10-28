cc729ee8b089810beb4c9e4ba659a244cc401791
Murph <shmilyclxy@163.com>
2019-10-28 18:06:07 +0800

    feat: init

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Head from 'next/head'
 import Nav from '../components/nav'
 
 const Home = () => (
   <div>
-    <Nav ><div>123</div></Nav>
+    <Head>
+      <title>Home</title>
+      <link rel='icon' href='/favicon.ico' />
+    </Head>
+
+    <Nav />
 
     <div className='hero'>
       <h1 className='title'>Welcome to Next.js!</h1>
